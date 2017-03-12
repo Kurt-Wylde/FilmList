42395374436631b78e185db367954f130d82a6de
Kurt Wylde <wylde.kurt@gmail.com>
2017-03-12 23:42:33 +0300

    first commit tableView added and + button with alertVC

--- FilmList/ViewController.swift
+++ FilmList/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class tableViewController: UITableViewController {
+    
+    var listItems = [NSManagedObject]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(tableViewController.addItem))
+    }
+    
+    func addItem(){
+    
+        let alertController = UIAlertController(title: "Type Something!", message: "Type...", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let confirmAction = UIAlertAction(title: "Type Something!!!", style: UIAlertActionStyle.default, handler: ({
+            (_) in
+            if let field = alertController.textFields![0] as? UITextField {
+            
+                self.saveItem(itemToSave: field.text!)
+                self.tableView.reloadData()
+            }
+        }
+            
+        ))
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        
+        alertController.addTextField(configurationHandler: ({
+            (textField) in
+            textField.placeholder = "Type here something"
+        }))
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func saveItem(itemToSave : String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "ListItems", in: managedContext)
+        let item = NSManagedObject.init(entity: entity!, insertInto: managedContext)
+        
+        do {
+            try managedContext.save()
+            listItems.append(item)
+        }
+        catch {
+            print("Error")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +67,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
+        
+        return cell
+    }
 
 }
 
