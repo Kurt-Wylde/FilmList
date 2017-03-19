6e054ddb96853439cd13bba05d2f693477e09da7
Kurt Wylde <wylde.kurt@gmail.com>
2017-03-20 00:13:56 +0300

    Major improvements. No more CoreData. added Delete on swipe func, mark as done func.

--- FilmList/ViewController.swift
+++ FilmList/ViewController.swift
@@ -7,100 +7,160 @@
 //
 
 import UIKit
-import CoreData
 
-class tableViewController: UITableViewController {
+class tableViewController: UITableViewController
+{
+    private var filmItems = [FilmItem]()
     
-    var listItems = [NSManagedObject]()
-
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(tableViewController.addItem))
-    }
-    
-    func addItem(){
-    
-        let alertController = UIAlertController(title: "Movie Title:", message: "", preferredStyle: UIAlertControllerStyle.alert)
         
-        let confirmAction = UIAlertAction(title: "Confirm", style: UIAlertActionStyle.default, handler: ({
-            (_) in
-            if let field = alertController.textFields![0] as? UITextField {
-            
-                self.saveItem(itemToSave: field.text!)
-                self.tableView.reloadData()
+        self.title = "Films to watch"
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tableViewController.didTapAddItemButton(_:)))
+        
+        // Setup a notification to let us know when the app is about to close,
+        // and that we should store the user items to persistence. This will call the
+        // applicationDidEnterBackground() function in this class
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(UIApplicationDelegate.applicationDidEnterBackground(_:)),
+            name: NSNotification.Name.UIApplicationDidEnterBackground,
+            object: nil)
+        
+        do
+        {
+            // Try to load from persistence
+            self.filmItems = try [FilmItem].readFromPersistence()
+        }
+        catch let error as NSError
+        {
+            if error.domain == NSCocoaErrorDomain && error.code == NSFileReadNoSuchFileError
+            {
+                NSLog("No persistence file found, not necesserially an error...")
+            }
+            else
+            {
+                let alert = UIAlertController(
+                    title: "Error",
+                    message: "Could not load the list items!",
+                    preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+                
+                NSLog("Error loading from persistence: \(error)")
             }
         }
-            
-        ))
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
-        
-        alertController.addTextField(configurationHandler: ({
-            (textField) in
-            textField.placeholder = "Type here..."
-        }))
-        alertController.addAction(confirmAction)
-        alertController.addAction(cancelAction)
-        
-        self.present(alertController, animated: true, completion: nil)
     }
     
-    func saveItem(itemToSave : String) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let entity = NSEntityDescription.entity(forEntityName: "ListEntity", in: managedContext)
-        let item = NSManagedObject.init(entity: entity!, insertInto: managedContext)
-        item.setValue(itemToSave, forKey: "item")
-        
-        do {
-            try managedContext.save()
-            listItems.append(item)
+    @objc
+    public func applicationDidEnterBackground(_ notification: NSNotification)
+    {
+        do
+        {
+            try filmItems.writeToPersistence()
         }
-        catch {
-            print("Error")
-        }
-        
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ListEntity")
-        do {
-            let results = try managedContext.fetch(fetchRequest)
-            listItems = results as! [NSManagedObject]
-        }
-        catch {
-            print("Error")
+        catch let error
+        {
+            NSLog("Error writing to persistence: \(error)")
         }
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let managedContext = appDelegate.persistentContainer.viewContext
-        tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.right)
-        managedContext.delete(listItems[indexPath.row])
-        listItems.remove(at: indexPath.row)
-        self.tableView.reloadData()
-    }
-    
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listItems.count
+    
+    func didTapAddItemButton(_ sender: UIBarButtonItem)
+    {
+        // Create an alert
+        let alert = UIAlertController(
+            title: "Movie Title:",
+            message: "",
+            preferredStyle: .alert)
+        
+        // Add a text field to the alert for the new item's title
+        alert.addTextField(configurationHandler: nil)
+        
+        // Add a "cancel" button to the alert. This one doesn't need a handler
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        // Add a "Confirm" button to the alert. The handler calls addNewFilmItem()
+        alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler:
+            { (_) in
+                // Get the title the user inserted, but only if it is not an empty string
+                if let title = alert.textFields?[0].text, title.characters.count > 0
+                {
+                    self.addNewFilmItem(title: title)
+                }
+        }))
+        
+        // Present the alert to the user
+        self.present(alert, animated: true, completion: nil)
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
-        let item = listItems[indexPath.row]
-        cell.textLabel?.text = item.value(forKey: "item") as? String
+    private func addNewFilmItem(title: String)
+    {
+        // The index of the new item will be the current item count
+        let newIndex = filmItems.count
+        
+        // Create new item and add it to the todo items list
+        filmItems.append(FilmItem(title: title))
+        
+        // Tell the table view a new row has been created
+        tableView.insertRows(at: [IndexPath(row: newIndex, section: 0)], with: .top)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return filmItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        if indexPath.row < filmItems.count
+        {
+            let item = filmItems[indexPath.row]
+            cell.textLabel?.text = item.title
+            
+            let accessory: UITableViewCellAccessoryType = item.done ? .checkmark : .none
+            cell.accessoryType = accessory
+        }
         
         return cell
     }
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row < filmItems.count
+        {
+            let item = filmItems[indexPath.row]
+            item.done = !item.done
+            
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if indexPath.row < filmItems.count
+        {
+            filmItems.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .top)
+        }
+    }
 }
-
