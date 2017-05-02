28f55fdb99876208bdba7a4ab2974a5ecff3ef22
Kurt Wylde <wylde.kurt@gmail.com>
2017-05-03 00:45:36 +0300

    watched preview

--- FilmList/Watched.swift
+++ FilmList/Watched.swift
@@ -58,5 +58,59 @@ class Watched: UITableViewController {
             }
         }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //setting the background image to the tableview
+        let backgroundImage = UIImage(named: "background.jpg")
+        let imageView = UIImageView(image: backgroundImage)
+        self.tableView.backgroundView = imageView
+        //hiding unusable lines in empty cells
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        // center and scale background image
+        imageView.contentMode = .scaleAspectFill
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        //Cell transparency
+        //cell.backgroundColor = .clear
+        //Cell text color
+        cell.textLabel?.textColor = .white
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return filmItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        if indexPath.row < filmItems.count
+        {
+            let item = filmItems[indexPath.row]
+            cell.textLabel?.text = item.title
+            
+//            let accessory: UITableViewCellAccessoryType = item.done ? .checkmark : .none
+//            cell.accessoryType = accessory
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if indexPath.row < filmItems.count
+        {
+            filmItems.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .top)
+        }
+    }
+
 
 }
